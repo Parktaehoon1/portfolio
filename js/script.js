55c0f7219620c2d6717a2bcbaed7f219632da27b
Parktaehoon1 <92parkth@gmail.com>
2022-07-23 11:19:32 +0900

    스와이퍼 코드 진행중

--- js/script.js
+++ js/script.js
@@ -1,19 +1,32 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
 })
 
-window.onload = function(){
+window.onload = function () {
     AOS.init();
-  
 
-//     $("#wrapper").dotdotdot({
-//         wrapper  : 'div',  /*  콘텐트를 감쌀 요소. */
-//         ellipsis: '... ',  /*  말줄임표를 뭘로 할지 */
-//         wrap  : 'word',    /*  자를 단위. 다음 옵션 중 하나 선택: 'word'/'letter'/'children' */
-//         after  : null,     /*  자르고 나서도 유지시킬 요소를 jQuery 선택자로 적는다. */
-//         watch  : false,    /*  윈도우가 리사이즈될 때 업데이트할 건지: true/'window' */
-//         height  : null,     /*  선택. max-height를 지정한다. 만약 null이면 알아서 잰다. */
-//         tolerance: 0       /*  글이 넘치면 이만큼쯤 height를 늘린다 */
-//       });
+
+    //     $("#wrapper").dotdotdot({
+    //         wrapper  : 'div',  /*  콘텐트를 감쌀 요소. */
+    //         ellipsis: '... ',  /*  말줄임표를 뭘로 할지 */
+    //         wrap  : 'word',    /*  자를 단위. 다음 옵션 중 하나 선택: 'word'/'letter'/'children' */
+    //         after  : null,     /*  자르고 나서도 유지시킬 요소를 jQuery 선택자로 적는다. */
+    //         watch  : false,    /*  윈도우가 리사이즈될 때 업데이트할 건지: true/'window' */
+    //         height  : null,     /*  선택. max-height를 지정한다. 만약 null이면 알아서 잰다. */
+    //         tolerance: 0       /*  글이 넘치면 이만큼쯤 height를 늘린다 */
+    //       });
+    new Swiper(".sw-visual", {
+		loop: true,
+		autoplay: {
+			delay: 1000,
+			disableOnInteraction: false,
+		},
+		speed: 1000,
+		pagination: {
+			el: '.swiper-pagination'
+		}
+    });
+
+
+
 }
-
