b59030b8e3a230e057f01cc46fc2d00b91cc7821
Parktaehoon1 <92parkth@gmail.com>
2022-08-05 18:08:29 +0900

    슬라이드 휠 추가

--- js/script.js
+++ js/script.js
@@ -3,6 +3,74 @@ $(document).ready(function () {
 })
 
 window.onload = function () {
+  // 스크롤바 
+  // 스크롤바 없애기
+  $('html').css('overflow', 'hidden');
+  // 1. section 을 저장한다.
+  let section = $('.wrap > section');
+  // 2. section 이동 속도
+  let sectionSpeed = 500;
+  // 3. section offset().top Y좌표값(px);
+  let sectionPos = [];
+  // 4. 화면에 보여지고 있는 번호
+  let sectionIndex = 0;
+  // 5. 총화면의 개수
+  let sectionTotal = section.length;
+  // 6. 연속 휠 이벤트 막기(true: 모션중, false: 모션완료)
+  let scrollIng = false;
+
+  // 화면의 사이즈가 변하면 100vh 가 달라진다.
+  // 화면의 사이즈가 변하면 sectionPos 도 달라져야 한다.
+  // sectionPos 는 화면의 각 이동 높이값 px 를 담아둔 것이다.
+  function resetSection(){
+    // sectionPos 값 넣기
+    $.each(section, function(index) {
+      // 웹브라우저 상단에서 얼마나 떨어져 있는가?
+      let tempY = $(this).offset().top;
+      sectionPos[index] = tempY;
+    });
+  }
+  // 한번은 화면을 셋팅한다.
+  resetSection();
+  $(window).resize(function(){
+    // 화면의 각 높이값을 갱신하였다.
+    resetSection();
+    // 화면의 위치를 맞춘다.
+    $('html').scrollTop(sectionPos[sectionIndex]);
+  });
+  // 마우스 휠 이벤트 체크
+  $(window).bind('mousewheel DOMMouseScroll', function (event) {    
+    let distance = event.originalEvent.wheelDelta;
+    if (distance == null) {
+      distance = event.originalEvent.detail * -1;
+    }
+    // 휠을 이용해서 화면이동을 하지않겠다.
+    if(scrollIng == true) {
+      return;
+    }
+    scrollIng = true;
+    if (distance < 0) {
+      sectionIndex ++;
+      if(sectionIndex >= sectionTotal) {
+        sectionIndex = sectionTotal - 1;
+      }
+    } else {
+      sectionIndex --;
+      if(sectionIndex < 0) {
+        sectionIndex = 0;
+      }
+    }
+    // 화면을 이동시킨다.
+    $('html').stop().animate({
+      scrollTop: sectionPos[sectionIndex]
+    }, sectionSpeed, function(){
+      // 애니메이션이 완료되었다.
+      // 다시 스크롤 이벤트를 받겠다.
+      scrollIng = false;
+    });
+  });
+
+
     AOS.init();
 
 
